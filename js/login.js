6a6d9a79b5f8677ac7bafa768286f33f78bf0694
kobirul5 <kobirul05j@gmail.com>
2024-09-18 21:42:53 +0600

    added condition for login

--- js/login.js
+++ js/login.js
@@ -1,1 +1,18 @@
-console.log('hello')
+// console.log('hello')
+document.getElementById('btn-login').addEventListener('click', function(event){
+    event.preventDefault();
+    // console.log('button click')
+    // step 2:- get phon number
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin').value;
+    // console.log(phoneNumber.value, pinNumber.value)
+    // step 3:- 
+    if(phoneNumber == '5' && pinNumber === '1234'){
+        console.log('login page')
+        
+    }
+    else{
+        alert('wrong phon number or pin number')
+    }
+
+})

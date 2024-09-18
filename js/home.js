79a002e0ce0e9db0d61723df40c32852c504d920
kobirul5 <kobirul05j@gmail.com>
2024-09-18 23:02:30 +0600

    added add money function with js

--- js/home.js
+++ js/home.js
@@ -1,1 +1,20 @@
-console.log("home added")
+document.getElementById('add-money-btn')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+
+        const addAmount = document.getElementById('input-add-amount').value;
+        const pinNumber = document.getElementById('pin-number').value;
+        const presentBalance = document.getElementById('present-balance').innerText;
+        console.log(addAmount, pinNumber, presentBalance)
+        if(pinNumber === '1234'){
+           const addAmountNumber = parseFloat(addAmount)
+           const balanceNumber = parseFloat(presentBalance)
+           const total = addAmountNumber + balanceNumber;
+           document.getElementById('present-balance').innerText = total;
+           document.getElementById('input-add-amount').value = '';
+           document.getElementById('pin-number').value = '';
+        }
+        else{
+            alert('Failed to add money, please ty again')
+        }
+    })

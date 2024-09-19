e300515a2e36277abcbf8a6ba384096d23044013
kobirul5 <kobirul05j@gmail.com>
2024-09-19 10:12:28 +0600

    added add money and cash out btn function

---
+++ js/feature.js
@@ -0,0 +1,8 @@
+document.getElementById('cash-out-option-btn').addEventListener('click', function(){
+    document.getElementById('cash-out-form').classList.remove('hidden')
+    document.getElementById('add-money-form').classList.add('hidden')
+})
+document.getElementById('add-money-option-btn').addEventListener('click', function(){
+    document.getElementById('cash-out-form').classList.add('hidden')
+    document.getElementById('add-money-form').classList.remove('hidden')
+})
